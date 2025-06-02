34a0e32eb2d497cfc81cae6ce304837cc29d5849
daumantasp <daumantasp@gmail.com>
2025-06-02 10:36:33 +0300

    Add BookmarkService interface and implementation.

---
+++ Bookmarker/Application/Services/BookmarkService.cs
@@ -0,0 +1,20 @@
+﻿using Bookmarker.Domain.Interfaces;
+using Bookmarker.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookmarker.Application.Services
+{
+    internal class BookmarkService(IBookmarkRepository repo) : IBookmarkService
+    {
+        private readonly IBookmarkRepository _repo = repo;
+
+        public async Task<IEnumerable<Bookmark>> GetAllAsync()
+        {
+            return await _repo.GetAll();
+        }
+    }
+}
---
+++ Bookmarker/Application/Services/IBookmarkService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bookmarker.Domain.Models;
+
+namespace Bookmarker.Application.Services
+{
+    internal interface IBookmarkService
+    {
+        Task<IEnumerable<Bookmark>> GetAllAsync();
+    }
+}
