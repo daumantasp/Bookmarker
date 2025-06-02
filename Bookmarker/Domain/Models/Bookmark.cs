399f09ba053ba470f9b7a6b5820dc6f0495ccb37
daumantasp <daumantasp@gmail.com>
2025-06-02 09:54:03 +0300

    Add Bookmark model.

---
+++ Bookmarker/Domain/Models/Bookmark.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookmarker.Domain.Models
+{
+    public class Bookmark(string Id,
+                          string Type,
+                          string Title,
+                          string Group,
+                          string Url,
+                          string[] Tags)
+    {
+        public string Id { get; } = Id;
+        public string Type { get; } = Type;
+        public string Title { get; } = Title;
+        public string Group { get; } = Group;
+        public string Url { get; } = Url;
+        public string[] Tags { get; } = Tags;
+    }
+}
