369c68c3fc5b5d01d41a39f1a28df33bf7494d21
daumantasp <daumantasp@gmail.com>
2025-06-02 09:56:56 +0300

    Add IBookmarkRepository interface.

---
+++ Bookmarker/Domain/Interfaces/IBookmarkRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bookmarker.Domain.Models;
+
+namespace Bookmarker.Domain.Interfaces
+{
+    internal interface IBookmarkRepository
+    {
+        Task<IEnumerable<Bookmark>> GetAll();
+    }
+}
