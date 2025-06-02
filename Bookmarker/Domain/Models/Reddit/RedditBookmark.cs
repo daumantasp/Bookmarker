442b80c6fe613968082ec8ac44f98a415f7a7106
daumantasp <daumantasp@gmail.com>
2025-06-02 10:14:23 +0300

    Implement RedditBookmarkRepository

---
+++ Bookmarker/Domain/Models/Reddit/RedditBookmark.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookmarker.Domain.Models
+{
+    public class RedditBookmark(string Id,
+                          string Type,
+                          string Title,
+                          string Subreddit,
+                          string Url,
+                          string[] Tags)
+    {
+        public string Id { get; } = Id;
+        public string Type { get; } = Type;
+        public string Title { get; } = Title;
+        public string Subreddit { get; } = Subreddit;
+        public string Url { get; } = Url;
+        public string[] Tags { get; } = Tags;
+
+        public Bookmark ToBookmark() => new Bookmark(
+                Id: Id,
+                Type: Type,
+                Title: Title,
+                Group: Subreddit,
+                Url: Url,
+                Tags: Tags
+            );
+    }
+}
---
+++ Bookmarker/Infrastructure/Repositories/Reddit/RedditBookmarkRepository.cs
@@ -0,0 +1,42 @@
+﻿using Bookmarker.Domain.Interfaces;
+using Bookmarker.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Bookmarker.Infrastructure.Repositories.Reddit
+{
+    internal class RedditBookmarkRepository(string filePath) : IBookmarkRepository
+    {
+        private readonly string _filePath = filePath;
+
+        public async Task<IEnumerable<Bookmark>> GetAll()
+        {
+            if (!File.Exists(_filePath))
+                throw new FileNotFoundException("Bookmark file not found.", _filePath);
+
+            try
+            {
+                var json = await File.ReadAllTextAsync(_filePath);
+                var serializerOptions = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                var redditBookmarks = JsonSerializer
+                    .Deserialize<IEnumerable<RedditBookmark>>(json, serializerOptions)
+                    .Select(rb => rb.ToBookmark());
+
+                return redditBookmarks ?? Enumerable.Empty<Bookmark>();
+
+            }
+            catch (JsonException ex)
+            {
+                throw new Exception($"Error deserializing josn file: {ex.Message}", ex);
+            }
+
+        }
+    }
+}
