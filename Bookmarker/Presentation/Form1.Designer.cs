b478f64ea05e6da2ea850e8e48c791afbee684b4
daumantasp <daumantasp@gmail.com>
2025-06-03 10:51:34 +0300

    Auto size columns.

--- Bookmarker/Presentation/Form1.Designer.cs
+++ Bookmarker/Presentation/Form1.Designer.cs
@@ -48,6 +48,7 @@
             // 
             // dataGridView1
             // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Location = new Point(12, 41);
             dataGridView1.Name = "dataGridView1";
