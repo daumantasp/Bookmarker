6b1b3aa65dc1bf59398c9667fea50b701f365a38
daumantasp <daumantasp@gmail.com>
2025-06-03 10:19:50 +0300

    Load data on button click.

--- Bookmarker/Presentation/Form1.cs
+++ Bookmarker/Presentation/Form1.cs
@@ -1,4 +1,5 @@
 using Bookmarker.Application.Services;
+using Bookmarker.Presentation.ViewModels;
 
 namespace Bookmarker
 {
@@ -11,6 +12,34 @@ namespace Bookmarker
             InitializeComponent();
 
             _bookmarkService = bookmarkService;
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var bookmarks = await _bookmarkService.GetAllAsync();
+                foreach (var bookmark in bookmarks)
+                {
+                    Console.WriteLine($"Title: {bookmark.Title}, URL: {bookmark.Url}");
+                }
+
+                var id = 0;
+                var viewData = bookmarks.Select(b => new BookmarkGridModel(
+                    ++id, 
+                    b.Title, 
+                    String.Join(", ", b.Tags), 
+                    b.Group, 
+                    b.Type, 
+                    b.Url)
+                ).ToList();
+
+                dataGridView1.DataSource = viewData;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading bookmarks: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
