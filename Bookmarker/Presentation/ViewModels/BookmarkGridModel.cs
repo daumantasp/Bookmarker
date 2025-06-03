9300151e0301586d1925fe75d3dc6ed3df6731ef
daumantasp <daumantasp@gmail.com>
2025-06-03 10:19:31 +0300

    Add BookmarkGridModel.

---
+++ Bookmarker/Presentation/ViewModels/BookmarkGridModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookmarker.Presentation.ViewModels
+{
+    internal class BookmarkGridModel(
+        int id,
+        string title,
+        string tags,
+        string group,
+        string type,
+        string url)
+    {
+        public int Id { get; } = id;
+        public string Title { get; } = title;
+        public string Tags { get; } = tags;
+        public string Group { get; } = group;
+        public string Type { get; } = type;
+        public string Url { get; } = url;
+
+    }
+}
